905ac47a3dc96b1d9d27b20f78747d97645bb485
HuannAnd <huannvicente14@outlook.com>
2023-09-20 21:50:28 -0300

    creating frontend enviroment with Vite + JS + Tailwindcss

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,5 +1,46 @@
+from flask import Flask, jsonify
+from requests import request
 
-print( "poggers")
+print("change applied")
 
+app = Flask(__name__)
+
+onibus = [
+    {
+        'id': 1,
+        'linha': 'TI Rio Doce/ CDU',
+        'chegada': 10
+    },
+    {
+        'id': 2,
+        'linha': 'TI tancredo neves',
+        'chegada': 14
+    },
+    {
+        'id': 3,
+        'linha': 'circular',
+        'chegada': 24
+    },
+    {
+        'id': 4,
+        'linha': 'TI barro/ Macaxeira',
+        'chegada': 11
+    },
+
+]
+@app.route('/onibus', methods=['GET'])
+def obter_onibus():
+    return "hello world"
+
+# @app.route('/onibus/<int:id>', methods=['GET'])
+
+# def obter_onibus_por_id(id):
+#     for frota in onibus:
+#         if frota.get('id') == id:
+#             return jsonify(frota)
+
+# def editar_onibus_por_id(id):
+#     livro_alterado = request.get_json()
     
 
+app.run(port=5000, host='localhost')
--- frontend/main.js
+++
@@ -1,9 +0,0 @@
-async function getApiResponse() {
-    try {
-        const data = await fetch("http://localhost:3001/get-location")
-        const json = await data.json()
-    } catch (error) {
-        const err = error
-      console.error("error message")  
-    }
-}
